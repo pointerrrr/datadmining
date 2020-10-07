3e8e24b0dd34d881b05c46889adbf4140ad1ebc0
Raka Schipperheijn <raka@rpkschipperheijn.com>
2020-10-07 20:56:20 +0200

    Het werkt bijna kinda

--- tree.py
+++ tree.py
@@ -1,79 +1,99 @@
 import numpy as np
-from anytree import Node, RenderTree
+from anytree import AnyNode, RenderTree
 import random
+
 
 def tree_grow(x, y, nmin, minleaf, nfeat):
     for i in range(len(x)):
         x[i].append(y[i])
     goodNodes = 0
     for i in range(len(x)):
-        goodNodes += x[i][len(x[i] - 1)]
-    start = Node((goodNodes, len(y) - goodNodes, x))
-
+        goodNodes += x[i][-1]
+    start = AnyNode(tuple=(goodNodes, len(y) - goodNodes, x))
+    print(start.tuple)
     toDoNodes = [start]
 
     while len(toDoNodes) > 0:
         curNode = toDoNodes[0]
         del toDoNodes[0]
 
-        if len(curNode.item2) < nmin:
+        if len(curNode.tuple[2]) < nmin:
             continue
 
-        possibleSplits = random_unique_list(nfeat, len(curNode.item2))
+        possibleSplits = random_unique_list(nfeat, len(curNode.tuple[2][0]))
         splitValues = []
         for i in range(len(possibleSplits)):
-            splitValue = consider_splitt(curNode, possibleSplits[i])
+            splitValue = consider_split(curNode, possibleSplits[i])
             splitValues.append(splitValue)
-        max = (-1, -1, -1)
-        for i in range(len(splitValues)):
-            if splitValues[i].item2 > max.item2:
-                max = (splitValues[i], i)
-        
+        splitValues = sorted(splitValues, key=lambda tup: tup[1])
+       
         lList = []
         rList = []
-
-        for i in range(len(x)):
-            if x[i][max.item3] < max.item1:
-                lList.append(x[i])
-            else:
-                rList.append(x[i])
-
-
-
-        lNode = Node(((lList.count(1), lList.count(0)), lList), curNode)
-        rNode = Node(((rList.count(1), rList.count(0)), rList), curNode)
+        for splitValue in splitValues:
+            if splitValue[2] == -1:
+                continue
+            lList = []
+            rList = []
+            for i in range(len(x)):
+                if x[i][splitValue[2]] < splitValue[0]:
+                    lList.append(x[i])
+                else:
+                    rList.append(x[i])
+            if len(lList) >= minleaf and len(rList) >= minleaf:
+                break
+        if len(lList) <= 0 and len(rList)<= 0:
+            raise Exception("Iets fucked up met de lengte")
+        ll, rl = getClassDistribution(lList)
+        lr, rr = getClassDistribution(rList)
+        lNode = AnyNode(tuple=(ll, rl, lList), parent=curNode)
+        rNode = AnyNode(tuple=(lr, rr, rList), parent=curNode)
 
         toDoNodes.append(lNode)
         toDoNodes.append(rNode)
         
         curNode = 0
 
-    return blob
+    return start
+
+def getClassDistribution(lijst):
+    goodNodes = 0
+    for i in range(len(x)):
+        goodNodes += x[i][-1]
+    return (goodNodes, len(x)-goodNodes)
+
+def consider_split(node, splitIndex):
+    splitIndex = int(splitIndex)
+    x = node.tuple[2].copy()
+    print(splitIndex)
+    x = sorted(x, key=lambda z : z[splitIndex])
+    splits = findSplits(x, splitIndex)
+    if len(splits) == 0:
+        return (-1,-1,-1)
+    ln, rn = getClassDistribution(node.tuple[2])
+    iT = impurity((ln,rn))
+    splitValues = []
+    for i in splits:
+        l = x[0:i]
+        ld, rd = getClassDistribution(l)
+        iL = impurity((ld, rd))
+        piL = len(l) / len(x)
+        r = x[i:]
+        lr, rr = getClassDistribution(r)
+        iR = impurity((lr, rr))
+        piR = len(r) / len(x)
+        splitValues.append(iT - (piL * iL + piR * iR))
+    max = (-1, -1)
+    for i in range(len(splitValues)):
+        if splitValues[i] > max[1]:
+            max = (i, splitValues[i])
+
+    return ((x[splits[max[0]]][splitIndex]+ x[splits[max[0]] - 1][splitIndex] ) / 2, max[0], splitIndex)
 
 def tree_spliit(node):
 
     return 0
 
-def consider_spliit(node, splitIndex):
-    x = node.item2.copy()
-    sorted(x, key=lambda z : z[splitIndex])
-    splits = findSplits(x, splitIndex)
-    iT = impurity(node)
-    splitValues = []
-    for i in splits:
-        l = x[0:i]
-        iL = impurity(l.count(1), l.count(0), l)
-        piL = len(l) / len(x)
-        r = x[i:]
-        iR = impurity((r.count(1), r.count(0)), r)
-        piR = len(r) / len(x)
-        splitValues.append(iT - (piL * iL + piR * iR))
-    max = (-1, -1)
-    for i in range(len(splitValues)):
-        if splitValues[i] > max.item2:
-            max = (i, splitValues[i])
 
-    return ((x[splits[max.item1]] + x[splits[max.item1] - 1] ) / 2, max.item2)
 
 def findSplits(x, splitIndex):
     splits = []
@@ -87,19 +107,32 @@ def findSplits(x, splitIndex):
     return splits
 
 def impurity(node):
-    return (node.item1 / (node.item1 + node.item2)) * (node.item2 / (node.item1 + node.item2))
+    return (node[0] / (node[0] + node[1])) * (node[1] / (node[0] + node[1]))
 
 
-def random_unique_list(length, bound):
+def random_unique_list(length, upperbound):
     result = np.empty(length)
     for i in range(length):
-        next = random.randint(bound)
-        while result.contains(next):
-            next = random.randint(bound)
-        result[i] = next
+        next = random.randint(0, upperbound-1)
+        while np.isin(next, result):
+            next = random.randint(0, upperbound-1)
+        result[i] = int(next)
     return result
 
 def tree_pred(x, tr):
     return 0
 
-tree_grow(0,0,0,0,0)
+x= [
+    [22,0,0,28,1],
+    [46,0,1,32,0],
+    [24,1,1,24,1],
+    [25,0,0,27,1],
+    [29,1,1,32,0],
+    [45,1,1,30,0],
+    [63,1,1,58,1],
+    [36,1,0,52,1],
+    [23,0,1,40,0],
+    [50,1,1,28,0]
+    ]
+y = [0,0,0,0,0,1,1,1,1,1]
+tree_grow(x,y,0,0,5)
