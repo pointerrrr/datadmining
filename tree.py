941a4027f7757e2c9936ec30c1106a16294a7148
Gideon Ogilvie <g.a.ogilvie@students.uu.nl>
2020-10-09 19:50:17 +0200

    documentation

--- tree.py
+++ tree.py
@@ -15,7 +15,7 @@ import random
 #nfeat = total number of features to be considered for a split
 #returns a tree, where each node contains a
 #tuple (correctly classified (int), incorrectly classified(int), entries in this node (float[,]), classifier for entries int[]),
-#an int on what column the data was split and a float of the input data, where the data has been split according to the gini index until nmin or minleaf is violated
+#an int on what column the data was split and a float of the input data, where the data has been split according to the gini index
 def tree_grow(x, y, nmin, minleaf, nfeat):
     goodNodes = 0
     for i in y:
@@ -88,10 +88,10 @@ def tree_grow(x, y, nmin, minleaf, nfeat):
                 continue
 
         if len(lList) > 0:
-            ll, rl = getClassDistribution(lClassifier)
+            ll, rl = get_class_distribution(lClassifier)
             toDoNodes.append(AnyNode(tuple=(ll, rl, np.array(lList), np.array(lClassifier)), parent=curNode))
         if len(rList) > 0:
-            lr, rr = getClassDistribution(rClassifier)
+            lr, rr = get_class_distribution(rClassifier)
             toDoNodes.append(AnyNode(tuple=(lr, rr, np.array(rList), np.array(rClassifier)), parent=curNode))
         if len(lList) != 0 or len(rList) != 0:
             curNode.splitIndex = currentSplitIndex
@@ -100,13 +100,19 @@ def tree_grow(x, y, nmin, minleaf, nfeat):
     return start
 
 #tree_pred
-#x = 
+#x = 2d matrix of n by m, with n data entries of length m
+#tr = tree to classify data from x with
+#returns a vector of length n, with the predicted result for each entry of x
 def tree_pred(x, tr):
     predictionList = []
     for i in x:
         predictionList.append(prediction(i, tr))
     return predictionList
 
+#tree_grow_b
+#x, y, nmin, minleaf, nfeat are the same as for tree_grow
+#m = amount of samples to be drawn
+#returns a list of trees as resulting from tree_grow
 def tree_grow_b(x, y, nmin, minleaf, nfeat, m):
     result = []
 
@@ -123,6 +129,10 @@ def tree_grow_b(x, y, nmin, minleaf, nfeat, m):
 
     return result
 
+#tree_pred_b
+#x = 2d matrix of n by m, with data entries of length m
+#tr = list of trees to classify data from x with
+#returns a vector of length n, with the predicted result of each entry of x, using the trees of tr
 def tree_pred_b(x, tr):
     predLists = []
     for tree in tr:
@@ -141,12 +151,19 @@ def tree_pred_b(x, tr):
             result.append(0)
     return result
 
-def getClassDistribution(classifier):
+#get_class_distribution
+#classifier = int array containing zeros and/or ones
+#returns a (int, int) tuple, with the amount of left as the first item, and amount of zeros as second item
+def get_class_distribution(classifier):
     goodNodes = 0
     for i in classifier:
         goodNodes += i
     return (goodNodes, len(classifier) - goodNodes)
 
+#consider_split
+#node = node to consider a split on
+#splitIndex = column to split on
+#returns a tuple (float,float,int) value to split on, impurity reduction, column we split on
 def consider_split(node, splitIndex):
     #splitIndex is sometimes seen as float by python, because it is in a tuple with other floats, but it is always an int
     splitIndex = int(splitIndex)
@@ -164,10 +181,10 @@ def consider_split(node, splitIndex):
     x = x2
     x = np.array(sorted(x, key=lambda z : z[splitIndex]))
 
-    splits = findSplits(x, splitIndex)
+    splits = find_splits(x, splitIndex)
     if len(splits) == 0:
         return (-1,-1,-1)
-    ln, rn = getClassDistribution(node.tuple[3])
+    ln, rn = get_class_distribution(node.tuple[3])
     iT = impurity((ln,rn))
     splitValues = []
     for i in splits:
@@ -176,14 +193,14 @@ def consider_split(node, splitIndex):
         #classifiers for left node
         lc = x[:i,-1]
         #class distribution for left node
-        ld, rd = getClassDistribution(lc)
+        ld, rd = get_class_distribution(lc)
         #impurity of left node
         iL = impurity((ld, rd))
         piL = len(l) / len(x)
         #right node..
         r = x[i:, 0:-1]
         rc = x[i:, -1]
-        lr, rr = getClassDistribution(rc)
+        lr, rr = get_class_distribution(rc)
         iR = impurity((lr, rr))
         piR = len(r) / len(x)
         #..right node
@@ -199,8 +216,11 @@ def consider_split(node, splitIndex):
     return result
 
 
-#find all valid splits inside node
-def findSplits(x, splitIndex):
+#find_splits
+#x = 2d array of n by m, of which we want to find the possible valid splits
+#splitIndex = column to split on
+#returns all valid splits inside node
+def find_splits(x, splitIndex):
     splits = []
     prev = x[0]
     for i in range(1,len(x)):
@@ -210,9 +230,15 @@ def findSplits(x, splitIndex):
 
     return splits
 
+#impurity
+#node = node to calculate impurity of
+#returns the impurity of a node
 def impurity(node):
     return (node[0] / (node[0] + node[1])) * (node[1] / (node[0] + node[1]))
 
+#random_unique_list
+#length = length of resulting list
+#upperbound = upperbound of the values in resulting list (needs to be >= to length)
 #return a list of unique random integer between 0 and "upperbound" of "length"
 def random_unique_list(length, upperbound):
     result = np.empty(length)
@@ -229,62 +255,23 @@ def random_unique_list(length, upperbound):
         
     return result
 
-def prediction(entry, currentNode):
-
-    while len(currentNode.children) !=0:
-        currentSplitIndex = currentNode.splitIndex
-        currentSplitValue = currentNode.splitValue
+#prediction
+#entry = data entry to get prediction for
+#tree = tree used to classify entry
+#returns a prediction for entry (either 0 or 1)
+def prediction(entry, tree):
+    while len(tree.children) !=0:
+        currentSplitIndex = tree.splitIndex
+        currentSplitValue = tree.splitValue
         if entry[int(currentSplitIndex)] < currentSplitValue:
-            currentNode = currentNode.children[0]
+            tree = tree.children[0]
         else:
-            currentNode = currentNode.children[1]
+            tree = tree.children[1]
 
-    tup = currentNode.tuple
+    tup = tree.tuple
     if tup[0] == tup[1]:
         a = 0
     if tup[0] > tup[1]:
         return 1
     else:
         return 0
-
-c = np.loadtxt('eclipse-metrics-packages-2.0.csv', delimiter=';', skiprows=1, usecols=np.arange(2,44))
-y = c[:,1].astype(int)
-x = c[:,:]
-x = np.delete(x, 1, 1)
-y = list(map(lambda x : 1 if x > 0 else 0, y))
-tree = tree_grow(x, y, 15, 5, 41)
-
-d = np.loadtxt('eclipse-metrics-packages-3.0.csv', delimiter=';', skiprows=1, usecols=np.arange(2,44))
-e = d[:,1].astype(int)
-f = d[:,:]
-f = np.delete(f, 1, 1)
-e = list(map(lambda x : 1 if x > 0 else 0, e))
-#prediction = tree_pred(f, tree)
-#print('%.2f' % np.mean(e == prediction))
-
-result_b = tree_grow_b(x, y, 15, 5, 41, 100)
-prediction_b = tree_pred_b(f, result_b)
-
-
-
-TP = 0
-FP = 0
-TN = 0
-FN = 0
-
-"""for i in range(len(e)):
-    TP += 1 if prediction[i] == 1 and e[i] == 1 else 0
-    FP += 1 if prediction[i] == 1 and e[i] == 0 else 0
-    TN += 1 if prediction[i] == 0 and e[i] == 0 else 0
-    FN += 1 if prediction[i] == 0 and e[i] == 1 else 0"""
-
-for i in range(len(e)):
-    TP += 1 if prediction_b[i] == 1 and e[i] == 1 else 0
-    FP += 1 if prediction_b[i] == 1 and e[i] == 0 else 0
-    TN += 1 if prediction_b[i] == 0 and e[i] == 0 else 0
-    FN += 1 if prediction_b[i] == 0 and e[i] == 1 else 0
-
-print("TP:" + str(TP))
-print("FP:" + str(FP))
-print("TN:" + str(TN))
-print("FN:" + str(FN))
